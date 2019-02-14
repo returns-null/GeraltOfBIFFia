81adc56b341e8f6e00bc3fde65b9bfb1cffb1baa
returns-null <returns.null@gmail.com>
2019-02-14 07:50:01 -0800

    Added mass extract of .BIF nodes; double-click on node to export all asset files into selected folder.

--- BIFTool.cs
+++ BIFTool.cs
@@ -21,6 +21,7 @@ namespace WitcherBIFTool
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             openKeyFileDialog.InitialDirectory = pathContext;
+            openKeyFileDialog.Title = "Choose a Witcher 1 .KEY File...";
             openKeyFileDialog.ShowDialog();
         }
 
@@ -28,39 +29,27 @@ namespace WitcherBIFTool
         {
             
             TreeNode selNode = BIFTreeView.SelectedNode;
-            
-            if (selNode != null && selNode.Parent != null)
+
+            if (selNode != null && selNode.Parent == null)
             {
-                String BIFname = selNode.Parent.Text;
-                BIF_KEYTABLE_ENTRY nodeData =  ((BIF_KEYTABLE_ENTRY)selNode.Tag);
-                String RESname = nodeData.ResourceName;
+                saveResourceDialog.Title = "Choose an export directory - loose files will be exported here.";
+                saveResourceDialog.FileName = selNode.Text;
+                DialogResult dr = saveResourceDialog.ShowDialog();
 
-                try
+                if (dr == System.Windows.Forms.DialogResult.OK)
                 {
-                    MemoryStream ms = BIF_DATA.getFileDataFromBIF(pathContext + BIFname, nodeData.ResourceID);
 
-                    saveResourceDialog.FileName = BIF_Utility.makeNewResName(nodeData.ResourceName, nodeData.ResourceType);
-                    DialogResult dr = saveResourceDialog.ShowDialog();
-
-                    if (dr == System.Windows.Forms.DialogResult.OK)
+                    foreach(TreeNode child in selNode.Nodes)
                     {
-                        StreamWriter sw = new StreamWriter(new FileStream(saveResourceDialog.FileName, FileMode.Create));
-                        ms.Position = 0;
-                        BIF_Utility.StreamCopyTo(ms, sw.BaseStream);
-                        sw.Close();
+                        BIFSaveOps.ExtractResourceFromTVNode(selNode.Text, child, pathContext, null);
                     }
+
                 }
-                catch (FileNotFoundException fnf_ee)
-                {
-                    MessageBox.Show("There was a problem retreiving the file from the BIF archive: \r\n"
-                        + fnf_ee.Message + "\r\nHint: Voice localization files for The Witcher DLC (e.g. M1_3_00.bif ) are stored in the voices subdirectory.", 
-                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                catch (Exception ee)
-                {
-                    MessageBox.Show("There was a problem extracting the selected file: \r\n"
-                        + ee.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+            }
+            else if (selNode != null && selNode.Parent != null)
+            {
+
+                BIFSaveOps.ExtractResourceFromTVNode(selNode.Parent.Text,selNode, pathContext, saveResourceDialog);
 
             }
         }
@@ -96,4 +85,48 @@ namespace WitcherBIFTool
             }
         }
     }
+
+    class BIFSaveOps
+    {
+        public static void ExtractResourceFromTVNode(String BIFNameIn, TreeNode selNode, String pathContext, SaveFileDialog saveResourceDialog)
+        {
+            String BIFname = BIFNameIn;
+            BIF_KEYTABLE_ENTRY nodeData = ((BIF_KEYTABLE_ENTRY)selNode.Tag);
+            String RESname = nodeData.ResourceName;
+
+            try
+            {
+                DialogResult dr = DialogResult.Abort;
+
+                MemoryStream ms = BIF_DATA.getFileDataFromBIF(pathContext + BIFname, nodeData.ResourceID);
+
+                if( saveResourceDialog != null )
+                {
+                    saveResourceDialog.FileName = BIF_Utility.makeNewResName(nodeData.ResourceName, nodeData.ResourceType);
+                    dr = saveResourceDialog.ShowDialog();
+                }
+
+                if (saveResourceDialog == null || dr == System.Windows.Forms.DialogResult.OK)
+                {
+                    StreamWriter sw = new StreamWriter(new FileStream(BIF_Utility.makeNewResName(nodeData.ResourceName, nodeData.ResourceType), FileMode.Create));
+                    ms.Position = 0;
+                    BIF_Utility.StreamCopyTo(ms, sw.BaseStream);
+                    sw.Close();
+                }
+            }
+            catch (FileNotFoundException fnf_ee)
+            {
+                MessageBox.Show("There was a problem retreiving the file from the BIF archive: \r\n"
+                    + fnf_ee.Message + "\r\nHint: Voice localization files for The Witcher DLC (e.g. M1_3_00.bif ) are stored in the voices subdirectory.",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("There was a problem retreiving the file from the BIF archive: \r\n"
+                    + ee.Message + "",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
 }
